f8e17b9ca2e1da16059a7788326a88c9e8135b65
msirazu <thebdrazu@gmail.com>
2026-03-05 14:32:27 +0600

    category and all tree fethed successfully

---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>Green Earth</h1>
+<h2>Technology Used</h2>
+<h3>HTML5, CSS3, TailwindCSS, DaisyUI, JavaScript</h3>
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,56 @@
-console.log('hello')
+const fetchData = async() => {
+    const catPromise = await fetch('https://openapi.programming-hero.com/api/categories');
+    const catData = await catPromise.json();
+    loadCategoryData(catData.categories);
+
+    const treePromise = await fetch('https://openapi.programming-hero.com/api/plants');
+    const treeData = await treePromise.json();
+    loadAllTree(treeData.plants);
+}
+
+fetchData();
+
+loadCategoryData = (data) => {
+    console.log(data)
+    const categoryContainer = document.getElementById('category-container');
+    data.forEach(cat => {
+        const catBtn = document.createElement('button');
+        catBtn.classList.add('btn', 'btn-outline', 'capitalize', 'w-full');
+        catBtn.innerHTML = `
+        ${cat.category_name}
+        `
+        categoryContainer.append(catBtn);
+    });
+}
+
+loadAllTree = (data) => {
+const allTreeContainer = document.getElementById('all-tree-container');
+data.forEach(tree => {
+    console.log(tree)
+    const div = document.createElement('div');
+div.classList.add('card', 'bg-base-100', 'shadow-sm');
+div.innerHTML = `
+<div class="card bg-base-100">
+  <figure>
+    <img class='h-50 w-full' src=${tree.image}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      ${tree.name}
+      <div class="badge badge-secondary">NEW</div>
+    </h2>
+    <p>${tree.description}</p>
+    <div class="card-actions justify-start">
+      <div class="badge badge-outline">${tree.category}</div>
+    </div>
+    <div class="flex justify-around items-center">
+        <p class='font-bold text-xl'>$ <span>${tree.price}</span></p>
+        <button class="btn btn-accent capitalize">cart</button>
+    </div>
+</div>
+</div>
+`
+allTreeContainer.append(div);
+});
+}
