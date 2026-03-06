0369b57c4cae0f46fc47b58554210583c4362d0e
msirazu <thebdrazu@gmail.com>
2026-03-06 10:54:38 +0600

    add to cart functionality added

--- scripts/main.js
+++ scripts/main.js
@@ -10,21 +10,28 @@ const fetchData = async() => {
 
 fetchData();
 
-loadCategoryData = (data) => {
+const cartDes = document.getElementById('cart-des');
+const cartInfo = document.getElementById('cart-info');
+const categoryContainer = document.getElementById('category-container');
+const allTreeContainer = document.getElementById('all-tree-container');
+const totalCartPrice = document.querySelector('.total-cart-price');
+
+let cart = [];
+
+const loadCategoryData = (data) => {
     // console.log(data)
-    const categoryContainer = document.getElementById('category-container');
     data.forEach(cat => {
         const catBtn = document.createElement('button');
         catBtn.classList.add('btn', 'btn-outline', 'capitalize', 'w-full', 'cat-btn-all');
         catBtn.innerHTML = `
         ${cat.category_name}
         `
-        categoryContainer.append(catBtn);
+        categoryContainer.appendChild(catBtn);
+        catBtn.addEventListener('click', () => selectCategory(cat.id));
     });
 }
 
-loadAllTree = (data) => {
-const allTreeContainer = document.getElementById('all-tree-container');
+const loadAllTree = (data) => {
 data.forEach(tree => {
     // console.log(tree)
     const div = document.createElement('div');
@@ -46,11 +53,45 @@ div.innerHTML = `
     </div>
     <div class="flex justify-around items-center">
         <p class='font-bold text-xl'>$ <span>${tree.price}</span></p>
-        <button onclick="addToCart()" class="btn btn-accent capitalize">cart</button>
+        <button onclick="addToCart('${tree.name}', '${tree.price}')" class="btn btn-accent capitalize">cart</button>
     </div>
 </div>
 </div>
 `
-allTreeContainer.append(div);
+allTreeContainer.appendChild(div);
 });
+}
+
+const selectCategory = (id) => {
+  console.log(id)
+}
+
+const addToCart = (name, price) => {
+  const existingCart = cart.find(item => item.name === name);
+  if (existingCart) {
+    existingCart.quantity++;
+  } else {
+    const treeCartData = {
+    name,
+    price: Number(price),
+    quantity: 1
+  }
+  cart.push(treeCartData);
+  }
+  renderCart();
+}
+
+const renderCart = () => {
+  cartInfo.innerHTML = '';
+    let total = 0;
+  cart.forEach(item => {
+    const p = document.createElement('p');
+    p.classList.add('font-bold');
+    p.innerHTML = `
+    ${item.name} ${item.price} x ${item.quantity} = ${item.price * item.quantity}
+    `
+    cartInfo.appendChild(p);
+    total = total + (item.price * item.quantity);
+  })
+  totalCartPrice.textContent = `$${total}`;
 }
